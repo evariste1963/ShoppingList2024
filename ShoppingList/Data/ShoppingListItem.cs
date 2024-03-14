36f2d71bd0cb890a3a781b1cd1a54b14523157fb
evariste1963 <evariste@hotmail.co.uk>
2024-03-14 20:43:21 +0000

    tut4 19:19

---
+++ ShoppingList/Data/ShoppingListItem.cs
@@ -0,0 +1,9 @@
+﻿namespace ShoppingList.Data
+{
+    public class ShoppingListItem
+    {
+        public string? Item { get; set; } = string.Empty;
+
+        public bool IsDone { get; set; } = false;
+    }
+}
