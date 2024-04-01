cbb03e03e79ea21c7285f103dc8706748dfb6842
evariste1963 <evariste@hotmail.co.uk>
2024-04-01 16:25:34 +0100

    getting ready for drag and drop items to reorder list
    https://github.com/tesar-tech/DragAndDropList/blob/master/src/Pages/Example2.razor

--- ShoppingList/Data/ShoppingListItem.cs
+++ ShoppingList/Data/ShoppingListItem.cs
@@ -2,8 +2,11 @@
 {
     public class ShoppingListItem
     {
+        public int Order { get; set; }
         public string? Item { get; set; } = string.Empty;
 
         public bool IsDone { get; set; } = false;
+
+        public bool IsDragOver { get; set; }
     }
 }
